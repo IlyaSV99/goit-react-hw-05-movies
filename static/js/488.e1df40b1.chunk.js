fc2c2ce12156d3a364c0371cec3aa71898f35c8a
IlyaSV99 <93597026+IlyaSV99@users.noreply.github.com>
2023-01-18 20:45:04 +0000

    Deploying to gh-pages from @ IlyaSV99/goit-react-hw-05-movies@0323a9f318031ce77ed08efa15adf5047cea06d1 🚀

---
+++ static/js/488.e1df40b1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[488],{488:function(e,t,i){i.r(t),i.d(t,{default:function(){return a}});var r=i(4691),o="loader_box__zGRZa",s=i(184);var a=function(){return(0,s.jsx)("div",{className:o,children:(0,s.jsx)(r.s5,{strokeColor:"grey",strokeWidth:"5",animationDuration:"0.75",width:"96",visible:!0})})}}}]);
+//# sourceMappingURL=488.e1df40b1.chunk.js.map
